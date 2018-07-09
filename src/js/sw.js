0ecf72bd9da0d97b0d62582d5c5b7659dbe4114c
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-10 01:16:43 +0300

    updated cach

--- src/js/sw.js
+++ src/js/sw.js
@@ -90,7 +90,9 @@ self.addEventListener('install', function(e) {
     caches.open('airhorner').then(function(cache) {
       return cache.addAll([
         '/',
+        '/index.html',
         '/index.html?homescreen=1',
+        '/?homescreen=1',
         '/bundle.css',
         '/bundle.js',
         '/images'

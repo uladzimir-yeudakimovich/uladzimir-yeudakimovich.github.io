ac724adf8bb7544e2fbaeb76b3b31023ccd7f19f
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-10 01:02:11 +0300

    updated sw

--- src/js/sw.js
+++ src/js/sw.js
@@ -85,26 +85,19 @@
   };
 }());
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('airhorner').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/index.html?homescreen=1',
-        '/?homescreen=1',
-        '/bundle.css',
-        '/bundle.js'
-      ]);
+// example usage:
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('demo-cache').then(function(cache) {
+      return cache.put('/', new Response("From the cache!"));
     })
   );
- });
+});
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      return response || new Response("Nothing in the cache for this request");
     })
   );
 });

1738e4bce76469ab13966278694b7b534a0e871e
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-10 02:30:05 +0300

    updated sw

--- sw.js
+++ sw.js
@@ -265,5 +265,5 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request));
+  event.respondWith(fetch(event.request));
 });

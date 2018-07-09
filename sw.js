1aeb82544be410e1266311cc457046935df58f3b
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-10 01:36:16 +0300

    updated sw

--- sw.js
+++ sw.js
@@ -89,7 +89,13 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('airhorner').then(function(cache) {
       return cache.addAll([
-        '/bundle.js'
+        '/',
+        '/index.html',
+        '/index.html?homescreen=1',
+        '/?homescreen=1',
+        '/bundle.css',
+        '/bundle.js',
+        '/images'
       ]);
     })
   );

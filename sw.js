bfe0a875f75b25ab01bcc19dfdd34a316948d72c
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-10 03:02:41 +0300

    updated sw

--- sw.js
+++ sw.js
@@ -250,6 +250,20 @@ self.addEventListener('install', function(event) {
   );
  });
 
+ document.querySelector('.cache-article').addEventListener('click', function(event) {
+  event.preventDefault();
+  var id = this.dataset.articleId;
+  caches.open('mysite-article-' + id).then(function(cache) {
+    fetch('/get-article-urls?id=' + id).then(function(response) {
+      // /get-article-urls returns a JSON-encoded array of
+      // resource URLs that a given article depends on
+      return response.json();
+    }).then(function(urls) {
+      cache.addAll(urls);
+    });
+  });
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open('mysite-dynamic').then(function(cache) {

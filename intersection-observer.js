e43c2f72b40ffa022dd34b160ff7ffc8da71ea9e
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-14 15:54:11 +0300

    updated observer

--- intersection-observer.js
+++ intersection-observer.js
@@ -1,32 +1,25 @@
-var io = new IntersectionObserver(
-  entries => {
-    console.log(entries);
-  },
-  {
-    /* Using default options. Details below */
-  }
-);
-// Start observing an element
-// io.observe(element);
+var targetNode = document.getElementById('language-ru-main');
 
-// Stop observing an element
-// io.unobserve(element);
+// Options for the observer (which mutations to observe)
+var config = { attributes: true, childList: true, subtree: true };
 
-// Disable entire IntersectionObserver
-// io.disconnect();
+// Callback function to execute when mutations are observed
+var callback = function(mutationsList) {
+    for(var mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+            console.log('A child node has been added or removed.');
+        }
+        else if (mutation.type == 'attributes') {
+            console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+    }
+};
 
-new IntersectionObserver(entries => {/* … */}, {
-  // The root to use for intersection.
-  // If not provided, use the top-level document’s viewport.
-  root: null,
-  // Same as margin, can be 1, 2, 3 or 4 components, possibly negative lengths.
-  // If an explicit root element is specified, components may be percentages of the
-  // root element size.  If no explicit root element is specified, using a percentage
-  // is an error.
-  rootMargin: "0px",
-  // Threshold(s) at which to trigger callback, specified as a ratio, or list of
-  // ratios, of (visible area / total area) of the observed element (hence all
-  // entries must be in the range [0, 1]).  Callback will be invoked when the visible
-  // ratio of the observed element crosses a threshold in the list.
-  threshold: [0],
-});
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+observer.disconnect();

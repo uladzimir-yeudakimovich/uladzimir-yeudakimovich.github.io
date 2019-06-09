24927aa6177d7cb9fc2a931233707c79cdd93afb
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2019-06-09 15:43:11 +0300

    improved header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  data: Array<object> = [
+    { technology: "JavaScript", level: [1,2,3,4,5] },
+    { technology: "HTML5",      level: [1,2,3,4,5] },
+    { technology: "CSS3",       level: [1,2,3,4,5] },
+    { technology: "Angular",    level: [1,2,3,4] },
+    { technology: "AngularJS",  level: [1,2,3,4] },
+    { technology: "Angular Material", level: [1,2,3,4,5] },
+    { technology: "Bootstrap",  level: [1,2,3,4,5] },
+    { technology: "Webpack",    level: [1,2,3,4] },
+    { technology: "Git",        level: [1,2,3,4] }
+  ]
+}
